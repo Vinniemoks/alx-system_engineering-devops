1e131cd185b4144b26287296d644c2d034579f1c
Vincent Mokua <mokua.vinny@gmail.com>
2025-01-15 13:18:23 -0800

    iit

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,13 @@
-# Install puppet-lint
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem'
-}
+#!/usr/bin/pup
+## install flask using puppet
+#package { 'Flask':
+#  ensure          => '2.1.0',
+#    provider        => 'pip3',
+#      install_options => ['--upgrade'],
+#      }
+#
+#      package { 'Werkzeug':
+#        ensure          => '2.1.1',
+#          provider        => 'pip3',
+#            install_options => ['--upgrade'],
+#            }
